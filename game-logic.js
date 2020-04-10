c100f837fd71fc9ea52b38fc8ec0b069aea89259
roryellis <rdellis1@gmail.com>
2020-04-09 21:16:57 -0500

    Linked game-logic.js to index. Pseudo game process outline

---
+++ game-logic.js
@@ -0,0 +1,20 @@
+//On game start: light show.
+lightShow();
+
+//Modal with instructions and "Ready to play?" button
+
+//game generates 3 random moves
+
+//demonstrate moves
+
+//player inputs moves
+
+//time limit for input
+
+//check player input
+
+//if correct: light cycle, increment number of moves, increment level counter, add a move to array
+
+//if incorrect: game over modal
+
+//reset button
